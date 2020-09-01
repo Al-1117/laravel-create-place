a7fd9fa5dcf22b1ff100950c332fc1bde42ff9db
Al-1117 <alioune1117@gmail.com>
2020-09-01 08:30:38 +0200

    caricato dati dalla tabella

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -3,29 +3,31 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Student;
 
 class StudentController extends Controller
 {
   public function index(){
-    $students = [
-      [
-        'nome' => 'Marco',
-        'cognome' => 'Bianchi',
-      ],
-      [
-        'nome' => 'Giuseppe',
-        'cognome' => 'Verdi',
-      ],
-      [
-        'nome' => 'Leonardo',
-        'cognome' => 'Gialli',
-      ],
-      [
-        'nome' => 'Carlo',
-        'cognome' => 'Garibaldi',
-      ]
-    ];
+    // $students = [
+    //   [
+    //     'nome' => 'Marco',
+    //     'cognome' => 'Bianchi',
+    //   ],
+    //   [
+    //     'nome' => 'Giuseppe',
+    //     'cognome' => 'Verdi',
+    //   ],
+    //   [
+    //     'nome' => 'Leonardo',
+    //     'cognome' => 'Gialli',
+    //   ],
+    //   [
+    //     'nome' => 'Carlo',
+    //     'cognome' => 'Garibaldi',
+    //   ]
+    // ];
 
+    $students = Student::all();
     return view('students', [
       'students' => $students,
     ]);
